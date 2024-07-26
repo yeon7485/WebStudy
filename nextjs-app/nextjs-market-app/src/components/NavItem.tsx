bf575b83414b0fb805901f18eb30771245ee2451
seiyeon <wsx4379@naver.com>
2024-07-26 17:37:31 +0900

    market: Next Auth
    Next Auth 연결

--- nextjs-app/nextjs-market-app/src/components/NavItem.tsx
+++ nextjs-app/nextjs-market-app/src/components/NavItem.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Link from 'next/link';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function NavItem({ mobile }: { mobile?: boolean }) {
+  const { data: session, status } = useSession();
+  console.log({ session }, status);
   return (
     <ul
       className={`text-md justify-center flex gap-4 w-full items-center ${
@@ -14,12 +17,15 @@ export default function NavItem({ mobile }: { mobile?: boolean }) {
       <li className='py-2 text-center border-b-2 cursor-pointer'>
         <Link href='/user'>User</Link>
       </li>
-      <li className='py-2 text-center border-b-2 cursor-pointer'>
-        <button>Signout</button>
-      </li>
-      <li className='py-2 text-center border-b-2 cursor-pointer'>
-        <button>Signin</button>
-      </li>
+      {session?.user ? (
+        <li className='py-2 text-center border-b-2 cursor-pointer'>
+          <button onClick={() => signOut()}>Signout</button>
+        </li>
+      ) : (
+        <li className='py-2 text-center border-b-2 cursor-pointer'>
+          <button onClick={() => signIn()}>Signin</button>
+        </li>
+      )}
     </ul>
   );
 }
---
+++ nextjs-app/nextjs-market-app/src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,46 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient } from '@prisma/client';
+import Credentials from 'next-auth/providers/credentials';
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    Credentials({
+      name: 'market',
+      credentials: {
+        username: {
+          label: 'Username',
+          type: 'text',
+          placeholder: 'jsmith',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, request) {
+        const user = {
+          id: '1',
+          name: 'J Smith',
+          email: 'jsmith@example.com',
+        };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: 'jwt',
+  },
+};
+
+export default NextAuth(authOptions);
