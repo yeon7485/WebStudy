3be6e45759393c18dab879a2845ab2f80df515fa
seiyeon <wsx4379@naver.com>
2024-07-29 23:59:28 +0900

    market: Next Auth 접근 제어, 세션 데이터 토큰
    - 로그인 한 사람만 특정 경로에 접근하기
    - 세션 데이터에 토큰 데이터 넣어주기

--- nextjs-app/nextjs-market-app/src/pages/api/auth/[...nextauth].ts
+++ nextjs-app/nextjs-market-app/src/pages/api/auth/[...nextauth].ts
@@ -41,6 +41,18 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: 'jwt',
   },
+  callbacks: {
+    async jwt({ token, user }) {
+      console.log('token', token);
+      console.log('user', user);
+      return { ...token, ...user };
+    },
+    async session({ session, token }) {
+      console.log('@', session, token);
+      session.user = token;
+      return session;
+    },
+  },
 };
 
 export default NextAuth(authOptions);
